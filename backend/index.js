1904c6462e0cccfa66bc60edcc1b69a3908787a9
Muhammad Sarmad <i220941@nu.edu.pk>
2025-04-23 10:17:03 +0500

    Fixed Cors issue

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://webdeploymenttask-production.up.railway.app'
+  }));
               // allow cross-origin
 app.use(express.json());
 
